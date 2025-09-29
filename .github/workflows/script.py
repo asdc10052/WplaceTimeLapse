e3f885fcd93f6b8005a4653be4855c20a14777f0
암브로시아 <102816877+dndbxkwh@users.noreply.github.com>
2025-09-29 14:56:01 +0900

    Update script.py

--- .github/workflows/script.py
+++ .github/workflows/script.py
@@ -1,69 +1,72 @@
-import json, os, re, requests, shutil
+import aiohttp, asyncio, io, json, os, re, warnings
 from datetime import datetime, timezone, timedelta
-from PIL import Image
+from PIL import Image, ImageChops
 
 class WplaceTimeLapse:
     
     def __init__(self, target):
         self.setup(target)
-        self.download()
-        self.merge()
-        self.delete()
-        self.gif()
+        try:
+            asyncio.run(self.fetchs())
+            self.png()
+            self.gif()
+        except NotChanged:
+            print(f"> 동일한 이미지")
+        except Exception as e:
+            print(f"> 오류 발생: {e}")
     
     def setup(self, target):
+        self.timestamp = datetime.now(timezone(timedelta(hours=9))).strftime('%Y%m%d %H%M%S')
         self.dirname = target['name']
+        self.pngname = f"{self.dirname}/{self.timestamp}.png"
+        self.gifname = f"{self.dirname}/{self.dirname}.gif"
         os.makedirs(self.dirname, exist_ok=True)
-        os.makedirs(f'{self.dirname}/temp', exist_ok=True)
-        self.filename = f"{datetime.now(timezone(timedelta(hours=9))).strftime('%Y%m%d %H%M%S')}.png"
         start = re.search(r"\(Tl X: (\d*), Tl Y: (\d*), Px X: (\d*), Px Y: (\d*)\)", target['start'])
         end = re.search(r"\(Tl X: (\d*), Tl Y: (\d*), Px X: (\d*), Px Y: (\d*)\)", target['end'])
         self.tx1, self.ty1, self.px1, self.py1 = map(int, start.groups())
         self.tx2, self.ty2, self.px2, self.py2 = map(int, end.groups())
         self.txl, self.tyl = self.tx2 - self.tx1 + 1, self.ty2 - self.ty1 + 1
-
-    def download(self):
-        for x in range(self.tx1, self.tx2+1):
-            for y in range(self.ty1, self.ty2+1):
-                response = requests.get(f"https://backend.wplace.live/files/s0/tiles/{x}/{y}.png")
-                open(f"{self.dirname}/temp/{x}_{y}.png", "wb").write(response.content)
+        print(f"[{self.dirname}] ({self.txl}x{self.tyl})")
     
-    def merge(self):
-        canvas = Image.new("RGBA", (self.txl*1000, self.tyl*1000))
-        for x in range(self.tx1, self.tx2+1):
-            for y in range(self.ty1, self.ty2+1):
-                img = Image.open(f"{self.dirname}/temp/{x}_{y}.png").convert("RGBA")
-                canvas.paste(img, ((x-self.tx1)*1000, (y-self.ty1)*1000), img)
-        canvas = canvas.crop((self.px1, self.py1, (self.txl-1)*1000 + self.px2 + 1, (self.tyl-1)*1000 + self.py2 + 1))
-        canvas = canvas.convert("P", palette=Image.ADAPTIVE, colors=256)
-        canvas.save(f'{self.dirname}/{self.filename}', optimize=True)
+    async def fetchs(self):
+        async with aiohttp.ClientSession() as session:
+            async def fetch(x, y):
+                try:
+                    async with session.get(f"https://backend.wplace.live/files/s0/tiles/{x}/{y}.png") as response:
+                        response.raise_for_status()
+                        data = await response.read()
+                        return (x, y, Image.open(io.BytesIO(data)).convert("RGBA"))
+                except Exception:
+                    print(f"> 다운로드 실패 ({x}, {y})")
+                    return (x, y, None)
+            tasks = [fetch(x, y) for x in range(self.tx1, self.tx2 + 1) for y in range(self.ty1, self.ty2 + 1)]
+            self.imgs = await asyncio.gather(*tasks)
         
-    def delete(self):
-        shutil.rmtree(f'{self.dirname}/temp')
-    
-    def gif(self):
+    def png(self):
+        canvas = Image.new("RGBA", (self.txl*1000, self.tyl*1000), (0,0,0,0))
+        for x, y, img in self.imgs:
+            if img:
+                canvas.paste(img, ((x-self.tx1)*1000, (y-self.ty1)*1000), img)
+        canvas = canvas.crop((self.px1, self.py1, (self.txl-1)*1000+self.px2+1, (self.tyl-1)*1000+self.py2+1))
+        canvas = canvas.convert("P", palette=Image.ADAPTIVE, colors=64)
         png_files = sorted([f for f in os.listdir(self.dirname) if f.endswith(".png")])
-        frames = []
-        for fname in png_files:
-            im = Image.open(os.path.join(self.dirname, fname)).convert("RGBA")
-            bg = Image.new("RGBA", im.size, (0,0,0,0))
-            bg.paste(im, mask=im.split()[3])
-            im_p = bg.convert("P").quantize(colors=64, method=Image.MEDIANCUT)
-            frames.append(im_p)
-        frames[0].save(
-            f"{self.dirname}/{self.dirname}.gif",
-            save_all=True,
-            append_images=frames[1:],
-            duration=100,
-            loop=0,
-            optimize=True
-        )
+        if png_files:
+            if ImageChops.difference(Image.open(os.path.join(self.dirname, png_files[-1])), canvas).getbbox() is None:
+                raise NotChanged()
+        canvas.save(self.pngname)
+        print(f'> {self.pngname}')
+
+    def gif(self):
+        warnings.filterwarnings("ignore", message="Palette images with Transparency expressed in bytes should be converted to RGBA images")
+        frames = [Image.open(os.path.join(self.dirname, f)) for f in os.listdir(self.dirname) if f.endswith(".png")]
+        frames[0].save(self.gifname, save_all=True, append_images=frames[1:], duration=100, loop=0, transparency=1)
+        print(f'> {self.gifname}')
+
+class NotChanged(Exception):
+    pass
 
 if __name__ == "__main__":
-    targets = json.load(open('.github/workflows/targets.json', 'r'))['targets']
+    targets = json.load(open('.github/workflows/config.json', 'r'))['targets']
     for target in targets:
-        try:
-            if target:
-                WplaceTimeLapse(target)
-        except:
-            pass
+        if target['enabled']:
+            WplaceTimeLapse(target)
